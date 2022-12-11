b15be679d5b8bc460d9e42a8e377da88f49f6841
hp <lars75@inbox.ru>
2022-12-11 16:27:47 +0300

    Task177

--- task/src/main/java/ru/olegD/condition/Task77.java
+++ task/src/main/java/ru/olegD/condition/Task77.java
@@ -1,5 +1,6 @@
 package main.java.ru.olegD.condition;
 
+
 import java.util.Arrays;
 import java.util.HashSet;
 
@@ -7,25 +8,21 @@ public class Task77 {
     public static void diffNumAmount(int num) {
         String[] str = String.valueOf(num).split("");
         HashSet<String> s = new HashSet<>(Arrays.asList(str));
-        int a = str.length - s.size();
-        if (a == 0) {
-            System.out.println(3);
-        } else if (a == 2) {
-            System.out.println(0);
-        } else {
-            System.out.println(a);
+        switch (s.size()) {
+            case 1 -> System.out.println(0);
+            case 2 -> System.out.println(2);
+            case 3 -> System.out.println(3);
         }
     }
 
     public static void main(String[] args) {
-        diffNumAmount(225);
+        diffNumAmount(223);
     }
 }
 /*
 Количество разных цифр в трехзначном числе
-Ниже представлен метод diffNumAmount(int num),
-который принимает целое положительное трехзначное число.
+Ниже представлен метод diffNumAmount(int num), который принимает целое положительное трехзначное число.
 Задание: Написать код, который определяет и выводит в консоль количество разных цифр в этом числе.
 Например, для числа 225 вывод будет:
-1
+2
  */
